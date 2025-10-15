d94f20b699e1726408bf4eaeabfb7da26e526728
jandoh07 <150073132+jandoh07@users.noreply.github.com>
2025-10-15 22:51:45 +0000

    feat: add AddButton component and integrate into Goals page

--- src/app/(dashboard)/goals/page.tsx
+++ src/app/(dashboard)/goals/page.tsx
@@ -3,14 +3,16 @@ import React from "react";
 import GoalCard from "@/components/goals/goal-card";
 import GoalsHeader from "@/components/goals/goals-header";
 import MobileHeader from "@/components/layout/mobile/header";
+import AddButton from "@/components/ui/add-button";
 
 const Goals = () => {
   return (
-    <div className="max-w-7xl mx-auto p-4">
+    <div className="max-w-7xl h-full mx-auto p-4 relative">
       <MobileHeader title="Your Goals" />
       <AiCoachTip />
       <GoalsHeader />
       <GoalCard />
+      <AddButton />
     </div>
   );
 };
---
+++ src/components/ui/add-button.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Button } from "./button";
+import { Plus } from "lucide-react";
+
+interface AddButtonProps {
+  className?: string;
+  onClick?: () => void;
+}
+
+const AddButton: React.FC<AddButtonProps> = ({ className, onClick }) => {
+  return (
+    <div
+      className={cn("absolute right-5 bottom-23 md:bottom-5", className)}
+      onClick={onClick}
+    >
+      <Button
+        variant={"default"}
+        size={"icon-lg"}
+        className="rounded-full bg-accent text-accent-foreground hover:bg-accent/90 shadow-lg h-15 w-15"
+      >
+        <Plus className="size-8" />
+      </Button>
+    </div>
+  );
+};
+
+export default AddButton;
