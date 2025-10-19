2f87e95a30ee08f6da000706fab7c0015f9e48d2
jandoh07 <150073132+jandoh07@users.noreply.github.com>
2025-10-19 20:09:25 +0000

    feat: update MobileHeader layout; fixed header to the top of page

--- src/components/layout/mobile/header.tsx
+++ src/components/layout/mobile/header.tsx
@@ -7,12 +7,15 @@ interface MobileHeaderProps {
 
 const MobileHeader = ({ title }: MobileHeaderProps) => {
   return (
-    <div className="md:hidden flex items-center justify-between mb-4 pb-2 border-b border-b-muted">
-      <p className="text-lg font-semibold">{title}</p>
-      <div>
-        {/* <Bell className="size-5" /> */}
-        <ThemeToggle />
+    <div>
+      <div className="md:hidden flex items-center justify-between py-2 px-3 border-b border-b-muted fixed top-0 left-0 w-full bg-background">
+        <p className="text-lg font-semibold">{title}</p>
+        <div>
+          {/* <Bell className="size-5" /> */}
+          <ThemeToggle />
+        </div>
       </div>
+      <div className="w-full h-7 md:hidden"></div>
     </div>
   );
 };
